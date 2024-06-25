739d64935e702dbdd82ff783a414b8527f6cab74
Dhvani <drp9975@gmail.com>
2024-06-25 16:47:54 +0530

    P1-updated forgot password, reset password, banner on home page

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,41 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CarouselModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
-
-  isCarouselActive: boolean = false;
-  customOptions: OwlOptions = {
-    loop: true,
-    margin: 29,
-    nav: true,
-    navText: ['<ol class="carousel-indicators"> <li class="active"></li><li ></li><li></li></ol>'],
-    dots: false,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-        0:{
-            items:1
-        },
-        576:{
-            items:2
-        },
-        768:{
-            items:3
-        },
-        992:{
-            items:1
-        }
-    }
-  }
-
-  ngOnInit(): void {
-    
-  }
+export class HomeComponent{
 }
