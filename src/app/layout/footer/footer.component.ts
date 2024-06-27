64a80678dc048aa58415eaf6f1c5a71d2e00fadf
mkpatel-247 <meet.codes247@gmail.com>
2024-06-27 12:55:46 +0530

    #53 P1-Footer Quick links and newsletter form is set with validation

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsletterComponent } from './newsletter/newsletter.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, NewsletterComponent],
+  imports: [CommonModule, NewsletterComponent, RouterModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
