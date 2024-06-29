6d8355b2986f7ca163fdc1eb21541d6254948e64
Knight-03 <drp9975@gmail.com>
2024-06-29 23:30:25 +0530

    removed some unused code

--- src/app/main/home/featured-products/featured-products.component.ts
+++ src/app/main/home/featured-products/featured-products.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from '../../products/products.component';
-import { ProductCardComponent } from '../../product-grid/product-grid.component';
-import { CommonService } from 'src/app/shared/service/common.service';
 
 @Component({
   selector: 'app-featured-products',
   standalone: true,
-  imports: [CommonModule,ProductCardComponent],
+  imports: [CommonModule],
   templateUrl: './featured-products.component.html',
   styleUrls: ['./featured-products.component.scss']
 })
 export class FeaturedProductsComponent {
-  constructor(public commonService: CommonService) {
-    
-  }
+
 }
