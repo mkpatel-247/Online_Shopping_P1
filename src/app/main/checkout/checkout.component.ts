358a0bb912e4afb4b07e9d73ac5bf81a9798ec23
Knight-03 <drp9975@gmail.com>
2024-06-30 15:44:55 +0530

    Updated checkout form

--- src/app/main/checkout/checkout.component.ts
+++ src/app/main/checkout/checkout.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CheckoutFormComponent } from 'src/app/shared/components/checkout-form/checkout-form.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CheckoutFormComponent, RouterModule],
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent {
+  showShippingForm : boolean = false;
 
+  setShippingForm(checkbox : any) {
+    this.showShippingForm = checkbox.checked;
+    
+  }
 }
