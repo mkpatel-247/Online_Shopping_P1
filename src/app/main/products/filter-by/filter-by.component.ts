5bda7afb521803a22559c28241d7f0c57b5be807
mkpatel-247 <meet.codes247@gmail.com>
2024-07-08 10:08:37 +0530

    Added ngx-slider

--- src/app/main/products/filter-by/filter-by.component.ts
+++ src/app/main/products/filter-by/filter-by.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-filter-by',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxSliderModule],
   templateUrl: './filter-by.component.html',
   styleUrls: ['./filter-by.component.scss']
 })
-export class FilterByComponent { }
+export class FilterByComponent {
+  @Input() minValue = 99;
+  @Input() maxValue = 1000;
+  options: Options = {
+    floor: 0,
+    ceil: 500,
+    translate: (value: number): string => {
+      return '$' + value;
+    },
+    getSelectionBarColor: '#ffd333'
+  };
+}
