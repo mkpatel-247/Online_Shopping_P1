792f7492e6d20185d3cc8fb6ca61c2dab9d36835
Dhvani <drp9975@gmail.com>
2024-07-09 10:01:39 +0530

    updated interceptor from class to function for angular 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,17 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { ApiInterceptor } from './shared/interceptor/api.interceptor';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     importProvidersFrom(BrowserAnimationsModule),
-    provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ApiInterceptor,
-      multi: true,
-    },
+    provideHttpClient(withInterceptors([ApiInterceptor]))
   ],
 };
--- src/app/shared/interceptor/api.interceptor.ts
+++ src/app/shared/interceptor/api.interceptor.ts
@@ -1,40 +1,40 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
 import { ToastMessageService } from '../components/toast-message/toast-message.service';
 import { TOAST_ICON, TOAST_STATE } from '../constant/app.constant';
 import { ProductService } from '../service/product.service';
 
-@Injectable()
-export class ApiInterceptor implements HttpInterceptor {
+export const ApiInterceptor: HttpInterceptorFn = (request, next) => {
 
-  constructor(private router: Router, private authService: AuthService, private toast: ToastMessageService, private productService: ProductService) { }
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const toastService = inject(ToastMessageService);
+  const productService = inject(ProductService);
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const authToken = this.authService.getLoginTokenFromLocalStorage();
-    if (authToken) {
-      request = request.clone({
-        headers: request.headers.set('authorization', `Bearer ${authToken}`)
-      });
-    }
-    return next.handle(request).pipe(
-      catchError((error) => {
-        if (error.status === 401) {
-          localStorage.removeItem('loginToken');
-          localStorage.removeItem('userDetail');
-          this.authService.userDetail.next([]);
-          this.productService.cartItems.next(0);
-          this.productService.wishlistItems.next(0);
-          this.router.navigate(['/auth/login']);
-          return throwError(() => "Logout");
-        }
-        if (error.status === 400 && !request.url.includes('/cart')) {
-          this.toast.showToast(TOAST_ICON.dangerIcon, TOAST_STATE.danger, error.error.message);
-        }
-        return throwError(() => 'Unknown Error');
-      })
-    );
+  const authToken = authService.getLoginTokenFromLocalStorage();
+  if (authToken) {
+    request = request.clone({
+      headers: request.headers.set('authorization', `Bearer ${authToken}`)
+    });
   }
+  return next(request).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+        localStorage.removeItem('loginToken');
+        localStorage.removeItem('userDetail');
+        authService.userDetail.next([]);
+        productService.cartItems.next(0);
+        productService.wishlistItems.next(0);
+        router.navigate(['/auth/login']);
+        return throwError(() => "Logout");
+      }
+      if (error.status === 400 && !request.url.includes('/cart')) {
+        toastService.showToast(TOAST_ICON.dangerIcon, TOAST_STATE.danger, error.error.message);
+      }
+      return throwError(() => 'Unknown Error');
+    })
+  )
 }
